1b98320a0b8258d58b5edf17c1e0d5383009234b
Phil Swan <mail@philswan.co.uk>
2020-04-08 15:20:42 +0100

    add washing and bathing

--- app/routes.js
+++ app/routes.js
@@ -61,6 +61,9 @@ router.get('/v1/list', (req, res, next) => {
   if (req.query.managingtreatments) {
     req.session.sectionStatus.managingtreatments = req.query.managingtreatments
   };
+  if (req.query.washingandbathing) {
+    req.session.sectionStatus.washingandbathing = req.query.washingandbathing
+  };
   if (req.query.dressingandundressing) {
     req.session.sectionStatus.dressingandundressing = req.query.dressingandundressing
   };
